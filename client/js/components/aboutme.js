687ecfa88f4b00165a5cc3213f68b576274477ac
galit.moss <galitmoss@me.com>
2023-06-25 12:04:08 +1000

    changes to about me contents

--- client/js/components/aboutme.js
+++ client/js/components/aboutme.js
@@ -13,17 +13,14 @@ function aboutMe() {
                 If you ever want some vegan recommendations, some good grocery finds, happy to share!
             </li>
             <li>Love binge watching Law and Order - have done a watch through of every episode twice now. All spin
-                offs
-                but UK... SVU will always be my favourite. Stabler forever.
+                offs but UK... SVU will always be my favourite. Stabler forever.
             </li>
-            <li>I am a diehard NRL fan. My team are the Roosters, but I am a bit of a Broncos fan too. I love the
-                rugby league community & how it brings people together. On game day you'll catch me in head to toe
-                gear,
-                with some red & blue makeup and nails to match. </li>
-            <li>I've been learning Yiddish on Duolingo for over 400 days now. I should add a counter that updates
+            <li>I am a diehard NRL fan. My team are the Roosters, but I am a bit of a Broncos fan too (much to the disappointment of my family). I love the
+                rugby league community & how it brings people together. And I also love to match my makeup & nails to my game day attire.</li>
+            <li>I've been learning Yiddish on Duolingo for over 430 days now. I should add a counter that updates
                 on the daily to show off my streak, maybe one day.
             </li>
-            <li>I'm really passionate about recycling and one of my favourite things to do when I had weekends, was
+            <li>I'm really passionate about recycling and one of my favourite things to do when I had weekends and free time, was
                 to sort out recylcing through <a class="link" href="https://banish.com.au/pages/recycling-program"
                     target="_blank">Banish's BRAD
                     program</a>
