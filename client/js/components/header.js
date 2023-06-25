6f37b5a19c189f161685cf863893e2819988a639
galit.moss <galitmoss@me.com>
2023-06-25 11:59:04 +1000

    adding about me to index & function, minor bug fixes to projectsDisplay and css

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -10,12 +10,14 @@ function setHeaderHtml() {
 <nav class="navbar navbar-expand-lg border-bottom border-bottom-dark" data-bs-theme="dark">
     <div class="container-fluid">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item nav-link" onClick="aboutMe()">About Me</li>
             <li class="nav-item">
                 <a class="nav-link" href="https://www.linkedin.com/in/galitmoss/" target="_blank">LinkedIn</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="https://github.com/gal333t/" target="_blank">GitHub</a>
             </li>
+            <li class="nav-item nav-link" onClick="projectDisplay()">Projects</li>
         </ul>
     </div>
 </nav>`;
--- client/js/components/projectsDisplay.js
+++ client/js/components/projectsDisplay.js
@@ -1,6 +1,6 @@
 function projectDisplay() {
   const page = document.getElementById("page");
-  const footer = document.getElementsById("footer");
+  const footer = document.getElementById("footer");
   footer.innerHTML = `<p class="footer">Some of the text on this page was written with the help of ChatGPT 🫶🏼</p>`;
   console.log("projects js is working");
 }
