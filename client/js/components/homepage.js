306da245b36d13e88605c55ba1fb90137dbadaa3
galit.moss <galitmoss@me.com>
2023-08-05 10:36:12 +1000

    wording changes onthe about me

--- client/js/components/homepage.js
+++ client/js/components/homepage.js
@@ -10,12 +10,12 @@ function renderHomePage() {
                   experience and refined my organisational skills within this fast-paced industry. However, my journey has
                   recently taken an exhilarating turn as I have delved into the captivating realm of coding.</p>
               <p>Driven by a desire for more engaging and dynamic work, I've eagerly embraced the world of programming.
-                  Equipped with a diverse skill set that includes <em>Javascript, HTML, CSS, Python, Express.js, NodeJs,
+                  Equipped with a diverse skill set that includes <em>Javascript, HTML, CSS, Python, React, Express.js, NodeJs,
                       Postgres,and MongoDB</em>, I've honed my abilities to bring my ideas to life through code. As my
                   course slowly
                   comes to an end now, I am hoping to get some more of my own personal projects in here. Does this website
                   count as one? I have been experimenting with Bootstrap for this website, trying to teach myself
-                  something new as CSS can be quite daunting. How did I do?
+                  something new as CSS can be quite daunting. After adding in backend and new features, it's been a really great learning experience. How did I do?
               </p>
             <img src="https://i.ibb.co/TTWj67G/B7-F3-A81-D-1441-4-E8-C-8515-1614-F1-BF05-EC.jpg" alt="photo of me in New York City with the skyline behind me on a bright sunny day most notably with the Empire State Building on my right. Wearing a green tank top, oversized black over shirt and black jeans." class="photo-me">
   </div>
